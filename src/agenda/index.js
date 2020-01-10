bbc2323b432ef1e122afdc9130bf137ba77120cd
Vanilson Fernandes <vanilson.fernandes@gmail.com>
2020-01-10 13:33:46 -0300

    fix header size

--- src/agenda/index.js
+++ src/agenda/index.js
@@ -17,7 +17,7 @@ import ReservationsList from './reservation-list';
 import styleConstructor from './style';
 import {VelocityTracker} from '../input';
 
-const HEADER_HEIGHT = 115;
+const HEADER_HEIGHT = 120;
 const KNOB_HEIGHT = 24;
 //Fallback when RN version is < 0.44
 const viewPropTypes = ViewPropTypes || View.propTypes;
