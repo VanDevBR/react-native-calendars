2cae009b2e4ca591992cf11b322962be445cb4c5
Vanilson Fernandes <vanilson.fernandes@gmail.com>
2020-02-18 17:25:59 -0300

    fix typo

--- src/agenda/index.js
+++ src/agenda/index.js
@@ -400,7 +400,7 @@ export default class AgendaView extends Component {
     };
   }
 
-  goToToday = () =>{
+  goToday = () =>{
     const today = XDate().toString('yyyy-MM-dd');
     this.chooseDay(today, this.state.calendarScrollable);
   };
@@ -440,7 +440,7 @@ export default class AgendaView extends Component {
 
     return (
       <Animated.View style={[this.styles.todayButtonContainer, {transform: [{translateY: buttonY}]}]}>
-        <TouchableOpacity style={[this.styles.todayButton, this.props.todayButtonStyle]} onPress={this.goToToday} disabled={disabled}>
+        <TouchableOpacity style={[this.styles.todayButton, this.props.todayButtonStyle]} onPress={this.goToday} disabled={disabled}>
           <Animated.Image style={[this.styles.todayButtonImage, {opacity}]} source={buttonIcon}/>
           <Animated.Text allowFontScaling={false} style={[this.styles.todayButtonText, {opacity}]}>{today}</Animated.Text>
         </TouchableOpacity>
